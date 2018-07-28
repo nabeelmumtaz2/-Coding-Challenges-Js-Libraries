4e98774128d5c570fe513b810ba1b9971bf12827
Nabeel <38478892+nabeelmumtaz2@users.noreply.github.com>
2018-07-28 15:02:15 -0400

    Create Truncated string

---
+++ Truncated string/nab.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/voca/1.4.0/voca.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+ $("#html").text(v.truncate('The perfect solution for professionals who need to balance work, family, and career building.', 50));
+});
+</script>
+</head>
+<body>
+<div id="html"></div>
+</body>
+</html>
