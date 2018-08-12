10f22c63758732b77af7bb4fde8909012f7f6e92
Nabeel <38478892+nabeelmumtaz2@users.noreply.github.com>
2018-08-11 22:44:32 -0400

    Create Is the Number a Prime

---
+++ Is the Number a Prime/nab.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/5.0.4/math.min.js"></script>
+</head>
+<body>
+<script>
+function isPrime(num) {
+  return math.isPrime(num);
+}
+
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(3));
+console.log(isPrime(4));
+console.log(isPrime(5));
+console.log(isPrime(6));
+console.log(isPrime(7));
+console.log(isPrime(8));
+console.log(isPrime(9));
+console.log(isPrime(10));
+console.log(isPrime(11));
+console.log(isPrime(102));
+console.log(isPrime(103));
+
+
+</script>
+</body>
+</html>
